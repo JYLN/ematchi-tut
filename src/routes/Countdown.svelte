b735f522124afd54ecffba19c8d7f344db64c396
JYLN <14062951+JYLN@users.noreply.github.com>
2024-02-17 12:26:12 -0700

    :writing_hand: Build `Countdown` for displaying countdown timer and playing functionality, build initial logic for countdown timer

---
+++ src/routes/Countdown.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	export let remaining: number;
+	export let duration: number;
+</script>
+
+<div class="countdown">
+	<button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="#CCC"
+			stroke="#999"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="lucide lucide-pause-circle"
+		>
+			<circle cx="12" cy="12" r="10" /><line x1="10" x2="10" y1="15" y2="9" /><line
+				x1="14"
+				x2="14"
+				y1="15"
+				y2="9"
+			/>
+		</svg>
+	</button>
+
+	<div class="duration">
+		<div class="remaining" style="--p: {remaining / duration}"></div>
+	</div>
+</div>
+
+<style>
+	.countdown {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+	}
+
+	button {
+		font-size: 8em;
+		width: 1em;
+		height: 1em;
+	}
+
+	svg {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+	}
+
+	.duration {
+		flex: 1;
+		height: 1em;
+		background: #ccc;
+	}
+
+	.remaining {
+		position: absolute;
+		width: calc(100% * var(--p));
+		height: 100%;
+		background: #999;
+	}
+</style>
--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Countdown from './Countdown.svelte';
 	import Found from './Found.svelte';
 	import Grid from './Grid.svelte';
 	import { levels, type Level } from './levels';
@@ -9,6 +11,9 @@
 	let size: number = level.size;
 	let grid: string[] = createGrid(level);
 	let foundPairs: string[] = [];
+	let remaining: number = level.duration;
+	let duration: number = level.duration;
+	let playing: boolean = false;
 
 	function createGrid(level: Level) {
 		const copy = [...level.emojis];
@@ -25,10 +30,34 @@
 		pairs.push(...pairs);
 		return shuffle(pairs);
 	}
+
+	function countdown() {
+		const start = Date.now();
+		let remainingAtStart = remaining;
+
+		function loop() {
+			if (playing) return;
+
+			requestAnimationFrame(loop);
+
+			remaining = remainingAtStart - (Date.now() - start);
+
+			if (remaining <= 0) {
+				// TODO: game lost
+				playing = false;
+			}
+		}
+
+		loop();
+	}
+
+	onMount(countdown);
 </script>
 
 <div class="game">
-	<div class="info"></div>
+	<div class="info">
+		<Countdown {remaining} duration={level.duration} />
+	</div>
 
 	<div class="grid-container">
 		<Grid
@@ -58,12 +87,10 @@
 	.info {
 		width: 80em;
 		height: 10em;
-		background-color: purple;
 	}
 
 	.grid-container {
 		width: 80em;
 		height: 80em;
-		background-color: aqua;
 	}
 </style>
