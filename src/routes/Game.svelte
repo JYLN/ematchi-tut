54da01a3604728a36f1ddffb5baad1297d005002
JYLN <14062951+JYLN@users.noreply.github.com>
2024-02-17 11:25:10 -0700

    :writing_hand: Game board scaffolding and global styles

---
+++ src/routes/Game.svelte
@@ -0,0 +1,31 @@
+<script lang="ts"></script>
+
+<div class="game">
+	<div class="info"></div>
+
+	<div class="grid-container"></div>
+
+	<div class="info"></div>
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+
+	.info {
+		width: 80vmin;
+		height: 10vmin;
+		background-color: purple;
+	}
+
+	.grid-container {
+		width: 80vmin;
+		height: 80vmin;
+		background-color: aqua;
+	}
+</style>
